888f58bc88b9f983c86a6df02aaa93ec8479ab03
jthn46 <jthzhang@ucdavis.edu>
2019-07-08 13:13:05 -0700

    initializing android project

---
+++ app/src/main/java/com/example/jatuh/spaceinvador/Space_Invador.java
@@ -0,0 +1,12 @@
+package com.example.jatuh.spaceinvador;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Space_Invador extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
